f3a31482fcfd9a9a3e0a602d705ee3344a76c846
AKSHATSINGH262002 <157829533+AKSHATSINGH262002@users.noreply.github.com>
2024-06-22 14:17:26 +0530

    Update pre.py

--- pre.py
+++ pre.py
@@ -2,10 +2,10 @@
 
 from pytesseract import pytesseract 
 #import io
-from config import tesseract_exec_path
+
 
 #pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-pytesseract.pytesseract.tesseract_cmd = tesseract_exec_path
+
 def extract(image_path):
     #image=Image.open(image_path)
     text=pytesseract.image_to_string(image_path)
