a1acaf30379ef64c14fcc3229561646f56749d87
AKSHATSINGH262002 <157829533+AKSHATSINGH262002@users.noreply.github.com>
2024-06-22 14:52:26 +0530

    Update pre.py

--- pre.py
+++ pre.py
@@ -4,11 +4,11 @@ from pytesseract import pytesseract
 #import io
 
 
-#pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def extract(image_path):
     #image=Image.open(image_path)
-    text=pytesseract.image_to_string(image_path)
+    text=pytesseract.image_to_string(image_path,config='--psm 11')
     arr=text.split()
     return arr
         
